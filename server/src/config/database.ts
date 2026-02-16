271fa5d48a922ec131f2a4bcda6796ca30141f83
koam@maticui <koam@maticui.com>
2026-02-16 08:02:32 +0530

    add 404 and

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 import { env } from './env.js';
+import SupportMessage from '../models/SupportMessage.model.js';
 
 export async function connectDatabase(): Promise<void> {
   await mongoose.connect(env.MONGODB_URI);
